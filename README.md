ccdbfa8e71c910de878fcd7192237affd44c9aef
charlie <554486586@qq.com>
2023-10-24 21:37:23 +0800

    support simple user map

--- README.md
+++ README.md
@@ -10,8 +10,6 @@ Connection pool is a thread safe list of websocket conn
 
 > based on sync map,meanwhile use runtime.mutex to ensure atomic.
 
-## How to implement wait-mechanism?
-
 ## In user ws connection pool situation, how to kick off user?
 
 ## How to implement user login based on several platforms?
@@ -20,7 +18,6 @@ Connection pool is a thread safe list of websocket conn
 
 > store user conn
 > update user conn
-> get specific user conn pool
 > get user ws conn pool
 > delete user conn by remote url
 > delete all user conn
--- user_map.go
+++ user_map.go
@@ -1,21 +1,42 @@
 package websocket_conn_pool
 
-import "sync"
+import (
+	"github.com/gorilla/websocket"
+	"sync"
+	"time"
+)
 
 type WSUserConnPool struct {
 	Pool sync.Map
 }
 
-// store user conn
+type Client struct {
+	conn       GWebSocket
+	PlatformID int    `json:"platformID"`
+	UserID     string `json:"userID"`
+}
 
-// update user conn
+type GWebSocket struct {
+	protocolType     int
+	conn             *websocket.Conn
+	handshakeTimeout time.Duration
+}
 
-// get specific user conn pool
+// Set StoreUserConn store user conn
+func (p *WSUserConnPool) Set(userID string, v *Client) {
+	p.Pool.Store(userID, v)
+}
 
-// get user ws conn pool
+// Get specific user conn pool
+func (p *WSUserConnPool) Get(userID string) (v *Client) {
+	client, ok := p.Pool.Load(userID)
+	if !ok {
+		return nil
+	}
+	return client.(*Client)
+}
 
-// delete user conn by remote url
-
-// delete all user conn
-
-// delete user specific conn
+// Delete user specific conn
+func (p *WSUserConnPool) Delete(userID string) {
+	p.Pool.Delete(userID)
+}
